710cbf6dce321a24c1d40c89e71e7af7353e8579
DannyJoy1 <dmerino2235@gmail.com>
2023-05-11 12:38:19 -0500

    multi languaje

---
+++ app.js
@@ -0,0 +1,12 @@
+var check=document.querySelector(".check");
+check.addEventListener('click', idiom);
+
+function idiom(){
+    let id=check.checked;
+    if (id==false){
+        location.href="/index.html";
+
+    }else{
+        location.href="/en/index.html"
+    }
+}
