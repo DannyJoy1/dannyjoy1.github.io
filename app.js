e734b1e084df2cde71e2832bb6cd5999181bdf03
DannyJoy1 <dmerino2235@gmail.com>
2023-06-25 17:02:12 -0500

    format

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var check=document.querySelector(".check");
+var check = document.querySelector(".check");
 check.addEventListener('click', idiom);
 
-function idiom(){
-    let id=check.checked;
-    if (id==false){
-        location.href="/index.html";
+function idiom() {
+    let id = check.checked;
+    if (id == false) {
+        location.href = "/index.html";
 
-    }else{
-        location.href="/en/index.html"
+    } else {
+        location.href = "/en/index.html"
     }
 }
